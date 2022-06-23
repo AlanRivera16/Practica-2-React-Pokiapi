5bc99bc16f0079a3d415410c79a238472c817e7e
AlanRivera16 <rivera.alan.3m@gmail.com>
2022-06-23 04:28:49 -0500

    style: the size and proporties of the cards are ready, the main div was decorated, the selection of color it's working

--- src/App.js
+++ src/App.js
@@ -63,50 +63,56 @@ function App() {
       const url = data.url.split('/');
       axios.get(`https://pokeapi.co/api/v2/pokemon/${url[6]}`).then((value)=>{
         setOnePok((current) => [...current,value]);
+        console.log(onePokemon);
       });
     });
   }, [index]);
 
   return (
     <div className="App">
-     <h1 className='titulo'>POKE API</h1>
-      <hr></hr>
-      <button className='btn btn-outline-success' onClick={() => {setIndex(index - 10);}}>Back</button>
-      <button className='btn btn-outline-success' onClick={() => {setIndex(index + 10);}}>Next</button>
+     {/* <h1 className='titulo' ngClass="asd">POKE API</h1>
+      <hr></hr> */}
+      <ul class="nav bg-danger text-white justify-content-center">
+        
+        <li class="nav-item">
+          <h1 >POKE API PRÁCTICA 2</h1>
+        </li>
+        
+      </ul>
       <div className='grid'>
+      <button className='btn btn-outline-danger pst pst_back' onClick={() => {setIndex(index - 10);}}>Back</button>
+      <button className='btn btn-outline-danger pst pst_next' onClick={() => {setIndex(index + 10);}}>Next</button>
         {
           onePokemon.length > 0 ? onePokemon.slice(0, 10).map((value, index) => (    
-            // <div key={index} className="card">
-            //   <div className="card-content">
-            //     <div class="container">
-            //       <div class="row">
-            //         <div class="col-6">
-            //           Column
-            //         </div>
-            //         <div class="col-6">
-            //           Column
-            //         </div>
-            //       </div>
-            //     </div>                
-            //     <h4><b className='uppercase'>{value.data.name}</b></h4>
-            //     <img src={value.data.sprites.front_default} alt={value.data.name}/>
-            //   </div>
-            // </div>
-            <div key={index} className="bg-danger">
-              
-                <div class="container">
-                  <div class="row align-items-center">
-                    <div class="col-6 bg-primary">
-                      <h4><b className='uppercase'>{value.data.name}</b></h4>
+            <div key={index} className={[
+                'card custom',
+                // value.data.types[0].type.name=="water" ? 'card custom bg-b' : '',
+                value.data.types[0].type.name=="fire" ? 'card custom bg-r color-w' : (value.data.types[0].type.name=="water" ? 'card custom bg-b color-w' 
+                :(value.data.types[0].type.name=="grass" ? 'card custom bg-v color-w' :(value.data.types[0].type.name=="bug" ? 'card custom bg-a color-w' 
+                :(value.data.types[0].type.name=="ground" ? 'card custom bg-w color-w' :(value.data.types[0].type.name=="fighting" ? 'card custom bg-c color-w' 
+                :(value.data.types[0].type.name=="poison" ? 'card custom bg-m color-w' :(value.data.types[0].type.name=="fairy" ? 'card custom bg-rs color-w' 
+                :(value.data.types[0].type.name=="electric" ? 'card custom bg-el color-w' :(value.data.types[0].type.name=="ghost" ? 'card custom bg-dr color-w' 
+                :(value.data.types[0].type.name=="rock" ? 'card custom bg-g color-w' :(value.data.types[0].type.name=="psychic" ? 'card custom bg-ps color-w' :'card custom')))))))))))
+                 
+                ]}>
+                <div class="container h-custom">
+                  <div class="row align-items-center relative h-custom">
+                    <div class="col-9 custom-w">
+                      <h4 className=''><b className='uppercase'>{value.data.name}</b></h4>
+                      {/* <small>{value.data.types[0].type.name}</small> */}
+                      <small className='fs-ss '>HABILIDADES</small>
+                      <div className='d-flex mb-3'>
+                        <small className='m-1 fs-s'>{value.data.abilities[0].ability?value.data.abilities[0].ability.name:'N/A'}</small>
+                        <small className='m-1 fs-s'>{value.data.abilities[1]?value.data.abilities[1].ability.name: 'N/A'}</small>
+                      </div>
                     </div>
-                    <div className="col-6 bg-secondary">
-                      <img src={value.data.sprites.front_default} alt={value.data.name}/>
+                    <div className="col-3 px-0">
+                      <div className='h-custom'>
+                        <img src={value.data.sprites.front_default} alt={value.data.name}/>
+                      </div>
                     </div>
                   </div>
                 </div>                
-                
-                
-              
             </div>
           )): null
         }
