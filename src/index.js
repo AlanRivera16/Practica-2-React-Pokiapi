3a34edbfa448027f6e29a1ddce347ecd6f0cc39c
AlanRivera16 <rivera.alan.3m@gmail.com>
2022-07-15 22:10:30 -0500

    feat: the routing to the components was added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Navbar from './components/navbar/navbar'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Acerca from './components/acerca_de/acerca_de';
+import OnePokemon from './components/card/card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <div>
+    <Navbar />
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/pokemons' element={<App/>} />
+          <Route path='/matricula' element={<Acerca/>} />
+          <Route path='/pokemons/:pokemonID' element={<OnePokemon/>} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
